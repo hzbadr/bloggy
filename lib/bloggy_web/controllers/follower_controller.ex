1ea40b8bdb04e038ab678e6ec5fa31a74003a541
Hassan Zaki <hassan.zaki@badrit.com>
2018-01-17 15:42:42 +0200

    remove white spaces

--- lib/bloggy_web/controllers/follower_controller.ex
+++ lib/bloggy_web/controllers/follower_controller.ex
@@ -1,27 +1,27 @@
 defmodule BloggyWeb.FollowerController do
   use BloggyWeb, :controller
-  
+
   alias Bloggy.Follower
   alias Bloggy.Repo
 
   def index(conn, _params) do
-    followers = Repo.all(Follower) 
-    |> Repo.preload([:follower, :followee])  
+    followers = Repo.all(Follower)
+    |> Repo.preload([:follower, :followee])
     render(conn, followers: followers)
   end
 
   def create(conn, params) do
     Follower.changeset(%Follower{}, params)
     |> Repo.insert!
-    
+
     redirect(conn, to: article_path(conn, :index))
   end
-  
+
   def delete(conn, %{"id" => user_id}) do
     current_user = Coherence.current_user(conn)
     Bloggy.Content.followers?(current_user.id, user_id)
-    |> Repo.delete! 
+    |> Repo.delete!
     redirect(conn, to: article_path(conn, :index))
   end
-  
+
 end
